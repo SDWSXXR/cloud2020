ba9b4b84e7e6e1c6bcb45783771d368f05caca45
xxr <xxr@qq.com>
2020-07-22 16:11:46 +0800

    添加支付模块的文件

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public Payment getPaymentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void creatPayment(Payment payment) {
+
+    }
+}
