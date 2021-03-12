c002fa73e8c5ba44e39ad3558e9a1eac00cadd15
xxr <xxr@qq.com>
2021-03-12 18:16:05 +0800

    创建eurka服务器，并将80和8001服务注册进eurka

--- cloud-eurka-server7001/src/main/java/com/atguigu/springcloud/EurkaMain7001.java
+++ cloud-eurka-server7001/src/main/java/com/atguigu/springcloud/EurkaMain7001.java
@@ -2,17 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author xxr
  * @Description
- * @date 2021/3/12 13:45
+ * @date 2021/3/12 17:54
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurkaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurkaMain7001.class,args);
     }
 }
