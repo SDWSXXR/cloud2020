dda0d41f976347206d5121e4a93cbf9ba4a7779d
xxr <xxr@qq.com>
2021-03-12 17:13:56 +0800

    项目重构，简历commons通用类

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -7,10 +7,7 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -21,7 +18,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value="/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int create = paymentService.creatPayment(payment);
         log.info("*******插入结果："+create);
         if(create>0){
