b3467b1486f034f8e5002f92c5401fc77c52e09c
xxr <xxr@qq.com>
2021-03-12 16:31:31 +0800

    订单模块测试完成

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xxr
+ * @Description
+ * @date 2021/3/12 13:45
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
