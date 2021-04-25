e84fc20a6ad2ae818ea1a6ea5833dab24cea1529
xxr <xxr@qq.com>
2021-04-25 10:23:10 +0800

    111

---
+++ cloud-api-file/src/main/java/com/atguigu/cloud/FileMain.java
@@ -0,0 +1,16 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xxr
+ * @Description
+ * @date 2021/4/25 10:22
+ */
+@SpringBootApplication
+public class FileMain {
+    public static void main(String[] args) {
+        SpringApplication.run(FileMain.class,args);
+    }
+}
